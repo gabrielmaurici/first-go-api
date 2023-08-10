0aaafdd181ceade146017dbc87cf99b43908cd6c
gabriel <gabriel.maurici@hotmail.com>
2023-08-09 22:00:32 -0300

    feature: add dependências main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"database/sql"
+	"gabrielmaurici/first-go-api/internal/database"
 	"gabrielmaurici/first-go-api/internal/routes"
+	"gabrielmaurici/first-go-api/internal/usecase"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	r := chi.NewMux()
+	db, err := sql.Open("mysql", "")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
+	postDb := database.NewPostDb(db)
+	postUsecase := usecase.NewPostUseCase(postDb)
+	postRoutes := routes.NewRouterPost(postUsecase)
+
+	r := chi.NewMux()
 	r.Use(middleware.Logger)
-	r.Mount("/post", routes.RouterPost())
+	r.Mount("/post", postRoutes.AddHandlerPost())
 
 	http.ListenAndServe(":8080", r)
 }
