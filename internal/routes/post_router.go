44d3722c5faffd3b8a1362686288562ce775dce4
gabriel <gabriel.maurici@hotmail.com>
2023-08-10 20:57:30 -0300

    melhorias nas funções de rotas

--- internal/routes/post_router.go
+++ internal/routes/post_router.go
@@ -22,24 +22,62 @@ func (rp *RouterPost) AddHandlerPost() http.Handler {
 	r := chi.NewRouter()
 
 	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		var dto usecase.PostInputDto
-
-		err := json.NewDecoder(r.Body).Decode(&dto)
+		err := Create(rp, r)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
 		}
 
-		rp.PostUsecase.Create(dto)
+		w.WriteHeader(http.StatusCreated)
 	})
 
-	r.Get("/", Get)
+	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		post, err := Get(rp, r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+		}
+
+		w.Header().Add("Content-Type", "json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(post)
+	})
+
 	r.Put("/", Update)
 
 	return r
 }
 
-func Get(w http.ResponseWriter, r *http.Request) {
-	//TODO
+func Create(rp *RouterPost, r *http.Request) error {
+	var dto usecase.PostInputDto
+
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		return err
+	}
+
+	err = rp.PostUsecase.Create(dto)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Get(rp *RouterPost, r *http.Request) ([]byte, error) {
+	id := chi.URLParam(r, "id")
+
+	post, err := rp.PostUsecase.Get(id)
+	if err != nil {
+		return nil, err
+	}
+
+	postJson, err := json.Marshal(post)
+	if err != nil {
+		panic(err)
+	}
+
+	return postJson, nil
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
